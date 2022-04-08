e5319ca0f60fbefd0c12ab94314ba44c5c932f67
Zakella <zapolski.slava@gmail.com>
2022-04-08 16:28:33 +0300

    lesson 4.2

--- lesson_4/zapolski_lesson_4.py
+++ lesson_4/zapolski_lesson_4.py
@@ -2,10 +2,10 @@ import utils
 
 def main():
 
-    print( utils.currency_rates("USD", True))
-    print( utils.Decimal.from_float(utils.currency_rates("USD")))
-    print( utils.currency_rates("EUR"))
-    print( utils.Decimal.from_float(utils.currency_rates("EUR")))
+    print(utils.currency_rates("USD", True))
+    print(utils.Decimal.from_float(utils.currency_rates("USD")))
+    print(utils.currency_rates("EUR"))
+    print(utils.Decimal.from_float(utils.currency_rates("EUR")))
 
 
 
