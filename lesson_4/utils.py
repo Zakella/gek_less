3381c1f77e79f3ef50d010ce6cca47fef561109f
Zakella <zapolski.slava@gmail.com>
2022-04-11 11:30:19 +0300

    lesson 4.3

--- lesson_4/utils.py
+++ lesson_4/utils.py
@@ -1,27 +1,27 @@
 import requests
 import xml.etree.ElementTree as ET
-from decimal import Decimal
 from datetime import datetime
+from decimal import Decimal
 
 
-def currency_rates(code, return_date = False):
+def currency_rates(code, return_date=False, dec=False):
     response = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")
     if response.status_code == 200:
         root = ET.fromstring(response.text)
         dict_currency = {}
         for val in root.findall('Valute'):
-            dict_currency[val.find('CharCode').text.lower()] = {"Value": float(val.find('Value').text.replace(',', '.')),
-                                                        "NumCode": val.find('NumCode').text,
-                                                        "Nominal": val.find('Nominal').text,
-                                                        "Name": val.find('Name').text}
+            dict_currency[val.find('CharCode').text.lower()] = {
+                "Value": float(val.find('Value').text.replace(',', '.')),
+                "NumCode": val.find('NumCode').text,
+                "Nominal": val.find('Nominal').text,
+                "Name": val.find('Name').text}
     if code.lower() in dict_currency:
+        value = dict_currency[code.lower()]["Value"]
+        if dec:
+            value = Decimal.from_float(value)
+        date_time_obj = ""
         if return_date:
-           for date in root.attrib.values():
-              break
-           date_time_obj = datetime.strptime(date, "%m.%d.%Y")
-           Value = dict_currency[code.lower()]["Value"]
-           return f"{Value},{date_time_obj.date().today()}"
-        else:
-            return dict_currency[code.lower()]["Value"]
-
-
+            for date in root.attrib.values():
+                break
+            date_time_obj = datetime.strptime(date, "%m.%d.%Y").date().today()
+    return f"{value},{date_time_obj}"
--- lesson_4/zapolski_lesson_4.py
+++ lesson_4/zapolski_lesson_4.py
@@ -1,13 +1,8 @@
 import utils
 
-def main():
-
-    print(utils.currency_rates("USD", True))
-    print(utils.Decimal.from_float(utils.currency_rates("USD")))
-    print(utils.currency_rates("EUR"))
-    print(utils.Decimal.from_float(utils.currency_rates("EUR")))
-
-
 
 if __name__ == "__main__":
-    main()
+    print(utils.currency_rates("EUR", True, True))
+    print(utils.currency_rates("USD"))
+
+
