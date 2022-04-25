1238d1bc113ddb1b1f4361169bc5d5f7fb3d5147
zapolski.slava@gmail.com <philips99>
2022-04-25 18:54:52 +0300

    8.1

--- lesson 8/lesson_8_task_1.py
+++ lesson 8/lesson_8_task_1.py
@@ -1,33 +1,39 @@
 import re
 
-# pattern = re.compile(r'(\d{2,3}.)')
+#задание 1
+def email_parse(email):
+ e_pattern = re.compile(r'^\w{2,}@\w*\.\w{1,3}')
+ valid_email = e_pattern.findall(email)
+ if not valid_email:
+     raise ValueError("Not valid e-mail")
+ user_domain = "".join(valid_email).split("@")
+ return {"username":user_domain[0],
+         "domain":user_domain[1]}
 
-ip_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-date_patern = re.compile(r'\[(.*?)\]')
-request_pattern = re.compile(r'[A-Z]{3}\s')
-# response_pattern = re.compile(r'\"(GET|POST) )(?P<url>.+)(http\/1\.1")
-code_pattern = re.compile(r'.+HTTP\/1\.1" (\d{3})')
 
-response_pattern = re.compile(
-    r"""  (http\/1\.1")""",
-    re.IGNORECASE)
+#задание 2
+def parse_nginx():
 
-""
+    # разбиваю на блоки что бы можно было норм читать
+    ip_pattern = re.compile(r'(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    date_patern = re.compile(r'\[(.*)\]')
+    request_pattern = re.compile(r'GET')
+    response_pattern = re.compile(r'[^\d]\/\w+\/\w+')
+    code_pattern = re.compile(r'.+HTTP\/1\.1" (\d{3}\s\d{1,4})')
+    with open("nginx_logs", "r", encoding="utf-8") as nginx:
+        for line in nginx:
+            remote_addr = "".join(ip_pattern.findall(line))
+            request_datetime = "".join(date_patern.findall(line))
+            request_type = "".join(request_pattern.findall(line))
+            requested_resource = "".join(response_pattern.findall(line))
+            code_size = code_pattern.findall(line)
+            response_size = "".join(code_size).split()[-1]
+            code = "".join(code_size).split()[0]
+            text =  (f'{remote_addr}, {request_datetime}, {request_type}, {requested_resource}, ' \
+                   f'{code}, {response_size}')
+            print(text)
 
-with open("nginx_logs", "r", encoding="utf-8") as nginx:
-    for line in nginx:
-        remote_addr = ip_pattern.findall(line)
-        request_datetime = date_patern.findall(line)
-        request_type = request_pattern.findall(line)
-        requested_resource = response_pattern.findall(line)
-        code = code_pattern.findall(line)
-        print(remote_addr, request_datetime, request_type, requested_resource, code)
-        break
-        # print(lineformat.findall(line))
-        # for res in result_iter:
-        # print(res.groups())  # найденные группы
-        # print(res.group(0))  # всё, что на сработала регулярка
-        # # print(res.group(1))  # первая группа, нумерация с 1
-        # # print(res.group(2))
-        # # print(res.group(3))
-        # # print(res.groupdict())
+
+if __name__ == "__main__":
+    print(email_parse("someone@geekbrains.ru"))
+    parse_nginx()
